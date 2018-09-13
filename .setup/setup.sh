a57f5087873072741d8ec3f29dee6c09df463303
Hidenori MATSUKI <MATSUKI.Hidenori@gmail.com>
2018-09-14 00:13:30 +0900

    comment out unnecessary steps.

--- .setup/setup.sh
+++ .setup/setup.sh
@@ -77,11 +77,11 @@ fi
 mkdir -p ${USER_BIN_DIR}
 mkdir -p ${LOCAL_TMPDIR}
 
-# Install Packer
-# ToDo: update version
-if [[ 'Darwin' == $(uname -s) ]]; then
-  curl -L -o "${LOCAL_TMPDIR}/packer.zip" "https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_darwin_amd64.zip"
-else
-  curl -L -o "${LOCAL_TMPDIR}/packer.zip" "https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip"
-fi
-unzip "${LOCAL_TMPDIR}/packer.zip" -d "${USER_BIN_DIR}"
+## Install Packer
+## ToDo: update version
+#if [[ 'Darwin' == $(uname -s) ]]; then
+#  curl -L -o "${LOCAL_TMPDIR}/packer.zip" "https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_darwin_amd64.zip"
+#else
+#  curl -L -o "${LOCAL_TMPDIR}/packer.zip" "https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip"
+#fi
+#unzip "${LOCAL_TMPDIR}/packer.zip" -d "${USER_BIN_DIR}"
