604f936b0e8829b428cd6920fe988477faeb49ca
Hidenori MATSUKI <MATSUKI.Hidenori@gmail.com>
2018-10-08 02:45:40 +0900

    Create GOPATH.

--- setup/lib/setup.via.go-get.sh
+++ setup/lib/setup.via.go-get.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -eu
 
+export GOPATH="${HOME}/.go"
+mkdir -p $GOPATH/bin
+(
+cd $GOPATH;
+ln -s ~/Creations src
+)
+
 function __setup_via_go_get() {
   local pkgs=(
   "github.com/github/hub"
