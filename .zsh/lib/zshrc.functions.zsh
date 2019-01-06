58406ffde1bc0289352e9c0c2abfeab9583baa3b
Hidenori MATSUKI <MATSUKI.Hidenori@gmail.com>
2019-01-07 06:52:56 +0900

    Add `--with-code` option to go to repo function.

--- .zsh/lib/zshrc.functions.zsh
+++ .zsh/lib/zshrc.functions.zsh
@@ -20,16 +20,26 @@ function go-to-repository() {
   "usage: $0"
   "        [-h | --help]"
   "        [-T | --with-tmux]"
+  "        [-C | --with-code]"
   )
 
   # parse arguments
-  zparseopts -D -M -A opts -E -- h -help=h T -with-tmux=T
+  zparseopts -D -M -A opts -E -- h -help=h T -with-tmux=T C -with-code=C
   if [[ -n ${opts[(i)-h]} ]]; then
     printf "%s\n" "${usage_msgs[@]}"
     return;
   fi
   if [[ -n ${opts[(i)-T]} ]]; then
     with_new_tmux_session=true
+  fi
+  if [[ -n ${opts[(i)-C]} ]]; then
+    if (( $+commands[code] )); then
+      with_new_vscode_window=true
+    else
+      tput setaf 3 && \
+        printf "\"code\" command not found." && \
+        tput sgr0
+    fi
   fi
 
   # to repository
@@ -43,6 +53,11 @@ function go-to-repository() {
       name+="/$(basename $(cd ..; pwd))"  # e.g. "github.com/mazgi"
       name+="/$(basename $PWD)"           # e.g. "github.com/mazgi/.dotfiles"
       tmux new -s "${name//\./+}"         # e.g. "github+com/mazgi/+dotfiles"
+    fi
+
+    # (optionnal)open via vscode
+    if [[ $with_new_vscode_window = true ]] ; then
+      code .
     fi
   fi
 }
