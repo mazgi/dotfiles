a5839c4fe77d8e28b18f581e3b541a33c69747aa
Hidenori MATSUKI <MATSUKI.Hidenori@gmail.com>
2020-02-07 15:37:17 +0900

    remove old scripts

--- setup/lib/setup.install.zsh-completions.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -eu
-
-function __setup_install_zsh_completions() {
-  # Hub
-  curl -o $HOME/.zsh/completions/_hub -L 'https://raw.githubusercontent.com/github/hub/master/etc/hub.zsh_completion'
-}
